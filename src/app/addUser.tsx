e9ad9156d5f7651ae397bcdffd610805802f6d06
daichitakahashi <t.daichi.snd@gmail.com>
2023-08-28 13:18:54 +0900

    add path to add user

---
+++ src/app/addUser.tsx
@@ -0,0 +1,51 @@
+import { vValidator } from '@hono/valibot-validator';
+import * as TE from 'fp-ts/TaskEither';
+import * as f from 'fp-ts/function';
+import { type Hono } from 'hono';
+import { object, string, email, minLength, maxLength } from 'valibot';
+import { type addUser as addUserFunc } from '../user/command';
+
+const formWithError = (errorMessage?: string) => (
+  <form action="/users" method="post">
+    {errorMessage ? <b>{errorMessage}</b> : null}
+    <div>
+      <label htmlFor="userName">User name</label>
+      <input type="text" name="userName" id="userName" />
+    </div>
+    <div>
+      <label htmlFor="userEmail">Email of user</label>
+      <input type="email" name="userEmail" id="userEmail" />
+    </div>
+    <button type="submit">Add</button>
+  </form>
+);
+
+export const addUserForm = (app: Hono) =>
+  app.get('/users', (c) => c.html(formWithError()));
+
+export const addUser = (add: ReturnType<typeof addUserFunc>) => (app: Hono) =>
+  app.post(
+    '/users',
+    vValidator(
+      // TODO: Validation error message
+      'form',
+      object({
+        userName: string([minLength(5), maxLength(20)]),
+        userEmail: string([email()]),
+      }),
+    ),
+    async (c) => {
+      await c.req.formData();
+      const form = c.req.valid('form');
+      return await f.pipe(
+        add({
+          name: form.userName,
+          email: form.userEmail,
+        }),
+        TE.match(
+          (err) => c.html(formWithError(err.type)),
+          (createdUser) => c.redirect(`/users/${createdUser.id}`),
+        ),
+      )();
+    },
+  );
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,26 +5,34 @@ import { type Hono } from 'hono';
 import { UserStore } from '../infra/inmemory/user-command';
 import { UserQuery } from '../infra/inmemory/user-query';
 import * as command from '../user/command';
+import { addUserForm, addUser } from './addUser';
 import { userDetail } from './userDetail';
 import { userList } from './userList';
 
 export const registerRoutes = async (app: Hono) => {
   const query = new UserQuery();
   const store = new UserStore(query.capture);
-  const addUser = command.addUser(store.saveUser);
+  const addUserCommand = command.addUser(store.saveUser);
 
   await f.pipe(
     Ap.sequenceT(TE.ApplySeq)(
-      addUser({ name: 'user01', email: 'user01@example.com' }),
-      addUser({ name: 'user02', email: 'user02@example.com' }),
-      addUser({ name: 'user03', email: 'user03@example.com' }),
+      addUserCommand({ name: 'user01', email: 'user01@example.com' }),
+      addUserCommand({ name: 'user02', email: 'user02@example.com' }),
+      addUserCommand({ name: 'user03', email: 'user03@example.com' }),
     ),
     TE.match(
       (err) => {
         throw err;
       },
       // routes
-      () => f.pipe(app, userList(query), userDetail(store)),
+      () =>
+        f.pipe(
+          app,
+          userList(query),
+          userDetail(store),
+          addUserForm,
+          addUser(addUserCommand),
+        ),
     ),
   )();
 };
